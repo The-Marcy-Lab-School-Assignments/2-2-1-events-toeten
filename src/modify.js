3508b6de0e4bb723182af29cd8ee882b9ac37fba
toeten <n.santiago0228@gmail.com>
2024-05-14 20:12:14 -0400

    answered q 3

--- src/modify.js
+++ src/modify.js
@@ -19,14 +19,37 @@ const clickCounterHandler = (event) => {
   button.addEventListener('click',clicked)
 };
 
+////////////////////////////////////
 
 const p = document.getElementById("keydown-tracker")
 const handleKeydown = (event) => {
-  let letter = event.key.toUpperCase()
-  p.textContent = `You pressed Key${letter}`
+  const letter = event.key
+  p.textContent = `You pressed Key${letter.toUpperCase()}`
+};
+document.body.addEventListener('keydown', handleKeydown);
+
+
+/////////////////////////////////////
+
+const notInline = document.getElementById("inline-example");
+let clickCountInline = 0;
+
+const clickedInline = () => {
+  clickCountInline++
+  notInline.setAttribute("data-clicks", clickCountInline)
+  updateButtonInline()
 };
 
-document.body.addEventListener('keydown',handleKeydown);
+const updateButtonInline = () => {
+  if (clickCountInline === 1) {
+    notInline.textContent = `I've been clicked ${clickCountInline} time.`
+  } else {
+    notInline.textContent = `I've been clicked ${clickCountInline} times!`
+  }
+}
+
+notInline.addEventListener('click', clickedInline);
+
 
 const handleDelegation = (event) => {
   const resultSpan = document.querySelector('#delegation-result');
