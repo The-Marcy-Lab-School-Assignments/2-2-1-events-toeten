364e50781a663f5e24c45866b77d27eff90e7b6f
toeten <n.santiago0228@gmail.com>
2024-05-15 11:29:46 -0400

    answered q 9

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("add-one")
+const number = document.getElementById('results') 
+const increment = () => {
+  let currentValue = Number(number.textContent)
+  number.textContent = currentValue + 1
+};
+
+
+const addOne = (event) => {
+  button.addEventListener('click', increment)
+}
+
+addOne()
